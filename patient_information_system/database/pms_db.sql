d8ff103b3195523e5496b00afb2ab280e000b7da
johnsilcanares-byte <johnsilcanares@gmail.com>
2026-07-12 10:24:40 +0800

    Add files via upload

---
+++ patient_information_system/database/pms_db.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2026 at 04:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL,
+  `reason` text NOT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `admin_note` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicines`
+--
+
+CREATE TABLE `medicines` (
+  `id` int(11) NOT NULL,
+  `medicine_name` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `medicines`
+--
+
+INSERT INTO `medicines` (`id`, `medicine_name`) VALUES
+(1, 'Amoxicillin'),
+(4, 'Antibiotic'),
+(5, 'Antihistamine'),
+(6, 'Atorvastatin'),
+(3, 'Losartan'),
+(2, 'Mefenamic'),
+(7, 'Oxymetazoline');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicine_details`
+--
+
+CREATE TABLE `medicine_details` (
+  `id` int(11) NOT NULL,
+  `medicine_id` int(11) NOT NULL,
+  `packing` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `medicine_details`
+--
+
+INSERT INTO `medicine_details` (`id`, `medicine_id`, `packing`) VALUES
+(1, 5, '50'),
+(4, 6, '25'),
+(5, 3, '80'),
+(6, 2, '100'),
+(7, 7, '25'),
+(8, 1, '50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `patient_name` varchar(60) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `cnic` varchar(17) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `age` int(3) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_medication_history`
+--
+
+CREATE TABLE `patient_medication_history` (
+  `id` int(11) NOT NULL,
+  `patient_visit_id` int(11) NOT NULL,
+  `medicine_details_id` int(11) NOT NULL,
+  `quantity` tinyint(4) NOT NULL,
+  `dosage` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient_visits`
+--
+
+CREATE TABLE `patient_visits` (
+  `id` int(11) NOT NULL,
+  `visit_date` date NOT NULL,
+  `next_visit_date` date DEFAULT NULL,
+  `bp` varchar(23) NOT NULL,
+  `weight` varchar(12) NOT NULL,
+  `disease` varchar(30) NOT NULL,
+  `patient_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `display_name` varchar(30) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `profile_picture` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `display_name`, `user_name`, `password`, `profile_picture`) VALUES
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', '1656551981avatar.png ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_appointments_patient_id` (`patient_id`);
+
+--
+-- Indexes for table `medicines`
+--
+ALTER TABLE `medicines`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `medicine_name` (`medicine_name`);
+
+--
+-- Indexes for table `medicine_details`
+--
+ALTER TABLE `medicine_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_medicine_details_medicine_id` (`medicine_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patient_medication_history`
+--
+ALTER TABLE `patient_medication_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_patient_medication_history_patients_visits_id` (`patient_visit_id`),
+  ADD KEY `fk_patient_medication_history_medicine_details_id` (`medicine_details_id`);
+
+--
+-- Indexes for table `patient_visits`
+--
+ALTER TABLE `patient_visits`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_patients_visit_patient_id` (`patient_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `medicines`
+--
+ALTER TABLE `medicines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `medicine_details`
+--
+ALTER TABLE `medicine_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `patient_medication_history`
+--
+ALTER TABLE `patient_medication_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `patient_visits`
+--
+ALTER TABLE `patient_visits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `medicine_details`
+--
+ALTER TABLE `medicine_details`
+  ADD CONSTRAINT `fk_medicine_details_medicine_id` FOREIGN KEY (`medicine_id`) REFERENCES `medicines` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
